e92f342c4b0c4c2df4fd00ef1dbd57bd0c922585
Edward B <57622377+P4tt4te@users.noreply.github.com>
2023-03-08 15:30:05 +0100

    feat: init canvas drawing

--- src/components/Experience/scenes/RegionEntranceOne/RegionEntranceOne.jsx
+++ src/components/Experience/scenes/RegionEntranceOne/RegionEntranceOne.jsx
@@ -1,12 +1,17 @@
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
+import { PerspectiveCamera } from "@react-three/drei"
 import { RegionEntranceOneScene } from "./RegionEntranceOneScene"
 import { useFrame } from "@react-three/fiber"
 import { useRef } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { HotGround } from "../../effects/HotGround"
+import { changeNoLerp } from "../../../../store/reducers/userReducer"
 import { AdinkraOne } from "../../objects/interactive/AdinkraOne/AdinkraOne"
 import { MathUtils } from "three"
 
 export const RegionEntranceOne = () => {
+  const dispatch = useDispatch()
+  const switchLerp = (value) => dispatch(changeNoLerp(value))
+  const noLerp = useSelector((state) => state.user.noLerp)
   //<fog attach={"fog"} args={["orange",30,60]} />
   /*
   <ScreenQuad scale={200} rotation={[0,Math.PI/2,0]} position={[-200, 0, -10]}>
@@ -16,16 +21,18 @@ export const RegionEntranceOne = () => {
 
   const ref = useRef()
   useFrame((state) => {
-    ref.current.rotation.y = MathUtils.lerp(
-      ref.current.rotation.y,
-      -(state.mouse.x * Math.PI) / 20 + Math.PI / 3,
-      0.05
-    )
-    ref.current.rotation.x = MathUtils.lerp(
-      ref.current.rotation.x,
-      ((state.mouse.y * Math.PI) / 20),
-      0.05
-    )
+    if (!noLerp) {
+      ref.current.rotation.y = MathUtils.lerp(
+        ref.current.rotation.y,
+        -(state.mouse.x * Math.PI) / 20 + Math.PI / 3,
+        0.05
+      )
+      ref.current.rotation.x = MathUtils.lerp(
+        ref.current.rotation.x,
+        (state.mouse.y * Math.PI) / 20,
+        0.05
+      )
+    }
   })
 
   return (
@@ -43,7 +50,7 @@ export const RegionEntranceOne = () => {
         position={[-30, 1, 0]}
         rotation={[-Math.PI / 2, Math.PI / 6, Math.PI / 2]}
       />
-      <AdinkraOne />
+      <AdinkraOne switchLerp={switchLerp} />
       <ambientLight intensity={1} />
       <RegionEntranceOneScene />
     </>
